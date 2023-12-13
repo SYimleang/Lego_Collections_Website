8c6cba273223f7c4d73ed002144c254ed5624a67
Sasawat Yimleang <122139640+SYimleang@users.noreply.github.com>
2023-12-13 05:03:37 -0500

    Update server.js

--- server.js
+++ server.js
@@ -56,9 +56,9 @@ authData.initialize();
 
 // set up sequelize to point to our postgres database
 const sequelize = new Sequelize(
-    "SenecaDB", 
-    "SYimleang", 
-    "ET0qngXaA4vH", {
+    'SenecaDB', 
+    'SYimleang', 
+    'ET0qngXaA4vH', {
         host: 'ep-yellow-salad-27925306-pooler.us-east-2.aws.neon.tech',
         dialect: 'postgres',
         port: 5432,
